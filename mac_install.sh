19fbdc69933b6d1d8191e6980bba43365c9e5a88
Dalton Flanagan <dalton@fb.com>
2019-09-28 12:06:52 -0400

    Scope Mac-specific install script

--- mac_install.sh
+++ mac_install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [[ "$OSTYPE" != "darwin"* ]]; then
+  echo "This script is intended for MacOS. Are you sure this is MacOS?"
+  exit 1
+fi
+
 # cp -iv to (1) prompt before overwrite and (2) be verbose (https://linux.die.net/man/1/cp)
 # mkdir -p for "no error if existing, make parent directories as needed" (https://linux.die.net/man/1/mkdir)
 
