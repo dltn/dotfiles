d5e042fc880a303d336dd858d8a237ca99854078
Dalton Flanagan <dalton@fb.com>
2019-10-16 14:46:26 -0700

    add tpm to extras (and tidy)

--- extras_install.sh
+++ extras_install.sh
@@ -1,9 +1,13 @@
 # Download & install Nord theme (https://www.nordtheme.com)
 echo "❄️  If you'd like to install Nord theme on your terminal emulator: https://www.nordtheme.com/ports"
 
-echo "Installing Nord vim..."
-mkdir -p ~/.vim/colors
-curl https://raw.githubusercontent.com/arcticicestudio/nord-vim/master/colors/nord.vim > ~/.vim/colors/nord.vim
+NORD_VIM=~/.vim/colors/nord.vim
+if [ ! -f "$NORD_VIM" ]; then
+    echo "Installing Nord vim to {$NORD_VIM}..."
+    mkdir -p ~/.vim/colors
+    curl https://raw.githubusercontent.com/arcticicestudio/nord-vim/master/colors/nord.vim > $NORD_VIM
+fi
+
 if ! grep -q "colo nord" ~/.vimrc
 then
     echo "colo nord" >> ~/.vimrc
@@ -13,6 +17,14 @@ fi
 # TODO: Add better package updating in this script
 VIMWIKI_DIR=~/.vim/pack/plugins/start/vimwiki
 if [ ! -d "$VIMWIKI_DIR" ]; then
-    echo "Installing vimwiki..."
+    echo "Installing vimwiki to {$VIMWIKI_DIR}..."
     git clone https://github.com/vimwiki/vimwiki.git $VIMWIKI_DIR
+fi
+
+# Download & install tpm (tmux plugin manager)
+# TODO: Add better package updating in this script
+TPM_DIR=~/.tmux/plugins/tpm
+if [ ! -d "$TPM_DIR" ]; then
+    echo "Installing tpm to {$TPM_DIR}..."
+    git clone https://github.com/vimwiki/vimwiki.git $TPM_DIR
 fi
